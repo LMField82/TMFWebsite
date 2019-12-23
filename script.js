c5dc96a37dcd97eccf90dd5cf0ec1ed9933042dd
LindsayMF <meneslindsay@hotmail.com>
2019-12-22 17:38:20 -0700

    added gallery.html filee with a carousel and a script.js file

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+  $(document).ready(function(){
+ 
+    $('#demo-carousel-auto').carousel();
+ 
+  setInterval(function() {
+ 
+    $('#demo-carousel-auto').carousel('next');
+ 
+  }, 2000);   
+ 
+   
 
 
+  });
